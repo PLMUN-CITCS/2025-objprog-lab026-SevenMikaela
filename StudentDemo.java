9ed8f57f78f74bf872e19476fa986adc919994c0
SevenMikaela <millionmikaelaseven_bsit@plmun.edu.ph>
2025-02-27 22:43:29 +0800

    Submitting OBJPROG Week 05 - Laboratory # 26

---
+++ StudentDemo.java
@@ -0,0 +1,51 @@
+public class StudentDemo {
+    public static void main(String[] args) {
+        
+        Student student1 = new Student("Alice", 101, 3.8);
+        Student student2 = new Student("Bob", 102, 3.5);
+        Student student3 = new Student("Charlie", 103, 3.9);
+
+     
+        student1.displayStudentInfo();
+        student2.displayStudentInfo();
+        student3.displayStudentInfo();
+        Student.displayStudentCount();
+    }
+}
+
+class Student {
+    private String name;
+    private int id;
+    private double gpa;
+
+    private static int studentCount = 0;
+
+    public Student(String name, int id, double gpa) {
+        this.name = name;
+        this.id = id;
+        this.gpa = gpa;
+        studentCount++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    // Method to display student information
+    public void displayStudentInfo() {
+        System.out.println("Student ID: " + id + ", Name: " + name + ", GPA: " + gpa);
+    }
+
+    // Static method to display total student count
+    public static void displayStudentCount() {
+        System.out.println("Total students: " + studentCount);
+    }
+}
